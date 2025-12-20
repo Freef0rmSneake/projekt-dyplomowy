332db1bba0a407106f8f88b5a9f54e33b4c9ecd4
Freef0rmSneake <dsw51560@student.dsw.edu.pl>
2025-12-20 15:39:30 +0100

    Konteneryzacja aplikacji

--- main.py
+++ main.py
@@ -79,4 +79,6 @@ def delete(index):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    debug_mode = os.getenv('FLASK_ENV') != 'production'
+    app.run(host='0.0.0.0', port=5000, debug=debug_mode)
