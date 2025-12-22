edd573110a8d781573982e62ac62fa50134be977
Freef0rmSneake <dsw51560@student.dsw.edu.pl>
2025-12-22 20:05:31 +0100

    Poprawka dziury znalezionej przez Bandit

--- main.py
+++ main.py
@@ -81,4 +81,6 @@ def delete(index):
 if __name__ == '__main__':
     import os
     debug_mode = os.getenv('FLASK_ENV') != 'production'
-    app.run(host='0.0.0.0', port=5000, debug=debug_mode)
+    host = os.getenv('FLASK_HOST', '127.0.0.1')
+    port = int(os.getenv('FLASK_PORT', '5000'))
+    app.run(host=host, port=port, debug=debug_mode)
